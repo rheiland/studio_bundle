8b809f687ad0de883fbbae9cd0edbe328b75cc86
Randy Heiland <randy.heiland@gmail.com>
2023-01-01 08:21:21 -0500

    update script

---
+++ scripts/update.sh
@@ -0,0 +1,10 @@
+cp ~/git/pmb-studio-dev/bin/pmb.py
+cp ~/git/pmb-studio-dev/bin/config_tab.py
+cp ~/git/pmb-studio-dev/bin/microenv_tab.py
+cp ~/git/pmb-studio-dev/bin/cell_def_tab.py
+cp ~/git/pmb-studio-dev/bin/user_params_tab.py
+cp ~/git/pmb-studio-dev/bin/ics_tab.py
+cp ~/git/pmb-studio-dev/bin/run_tab.py
+cp ~/git/pmb-studio-dev/bin/vis_tab.py
+cp ~/git/pmb-studio-dev/bin/legend_tab.py
+
