96f96138376b7e081d3ebfda2f37056f564f89e2
Randy Heiland <randy.heiland@gmail.com>
2023-01-02 05:20:10 -0500

    init

---
+++ scripts/cp_libs.sh
@@ -0,0 +1,9 @@
+# pushd ~/git/studio_bundle/scripts/dist/pmb.app/Contents/Frameworks
+# or,
+# pushd dist/pmb.app/Contents/Frameworks
+cp ~/opt/miniconda3/envs/myenv-conda/lib/libffi.7.dylib .
+cp ~/opt/miniconda3/envs/myenv-conda/lib/libiomp5.dylib .
+cp ~/opt/miniconda3/envs/myenv-conda/lib/libgfortran.5.dylib .
+cp ~/opt/miniconda3/envs/myenv-conda/lib/libquadmath.0.dylib .
+cp ~/opt/miniconda3/envs/myenv-conda/lib/libgcc_s.1.1.dylib .
+
